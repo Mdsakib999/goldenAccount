5e20617d5554d2035a73c944e94751bcb85510ec
Mdsakib999 <sakibabrar999@gmail.com>
2024-07-02 16:41:17 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className=''>
+        <Outlet></Outlet>
+      </div>
+  )
+}
+
+export default App

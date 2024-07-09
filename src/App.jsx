cafdabc3c1c19add73d8716c7a185a96ca6bc625
safi islam <118913244+safiislam@users.noreply.github.com>
2024-07-09 12:23:48 +0600

    order page complite

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import Footer from './components/Home/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className=''>
+
       <Outlet></Outlet>
+      <Footer />
     </div>
   )
 }

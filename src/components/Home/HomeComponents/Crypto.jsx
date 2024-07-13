f2ea1a04fa64c20e88355a50de561bad3c15829d
safi islam <118913244+safiislam@users.noreply.github.com>
2024-07-14 00:37:01 +0600

    add modal in crypto

--- src/components/Home/HomeComponents/Crypto.jsx
+++ src/components/Home/HomeComponents/Crypto.jsx
@@ -13,6 +13,7 @@ import { IoIosArrowDown } from "react-icons/io";
 const Crypto = () => {
   const [cards, setCards] = useState([]);
   const [modatData, setModalData] = useState({})
+  const [quantity, setQuantity] = useState(0)
   const [multimodalOpen, setMultimodalOpen] = useState(false)
   const [multidata, setMultiData] = useState({})
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -31,9 +32,9 @@ const Crypto = () => {
     setMultiData({})
     setMultimodalOpen(false)
     setDownState(false)
-
+    setQuantity(0)
   };
-
+  console.log(modatData);
   // DATA facing
   useEffect(() => {
     fetch("cryptoData.json")
@@ -46,7 +47,6 @@ const Crypto = () => {
       });
     //   .catch((error) => console.error('Error fetching data:', error));
   }, []);
-  console.log(multidata);
 
   return (
     <>
@@ -109,6 +109,7 @@ const Crypto = () => {
                     <p>${multidata.price} USD / {multidata.stock} in stock</p>
 
                   </div>
+                  {/* -----  arrow */}
                   <div>
                     {/* header text */}
                     {
@@ -175,15 +176,48 @@ const Crypto = () => {
                           }
                         </div>
                       )
-
-
                     }
+                    <Divider text={'ORDER'} />
+                    <div onClick={() => setDownState(!downState)} className="border flex justify-between items-center p-2 text-lg border-stone-400 mt-8 text cursor-pointer  rounded-md">
+                      <p >Choose one of {modatData?.cryptoData?.length} options</p>
+                      <IoIosArrowDown />
+                    </div>
+                    <div className=" mt-2">
+                      {
+                        downState && (
+                          <div className=" bg-[#11192C] p-4 rounded-md space-y-3">
+                            {
+                              modatData.cryptoData.map(item => <div onClick={() => {
+                                setMultiData({ ...item, cryptoTitle: modatData.cryptoTitle })
+                                setMultimodalOpen(true)
+                                setDownState(false)
+
+                              }} className=" cursor-pointer flex justify-between gap-2">
+                                <p className="">{item.title}</p>
+                                <span className={`flex h-[30px]  px-3 items-center justify-center border text-sm py-0 rounded-lg ${item.stock > 0 ? "text-green-600  border-green-600" : 'text-slate-500'}`}> <AiFillDollarCircle />{item.price}USD</span>
+                              </div>)
+                            }
+                          </div>
+                        )
+                      }
+                      {
+                        !downState && (
+                          <div>
+                            <div className="flex items-center">
+                              <span className="border border-stone-500 rounded-s-md py-2 px-4">Quantity</span>
+                              <input onChange={(e) => setQuantity(e.target.value)} type="number" className="bg-transparent ps-3 rounded-e-md focus:border-blue-500 outline-none w-full border-2 py-2" defaultValue={multidata.stock} placeholder="350" />
+                            </div>
+                            <button className="bg-blue-600 hover:bg-blue-500 w-full mt-4 py-2 rounded-md">Check out for {quantity > 1 ? quantity * multidata.price : multidata.price} </button>
+                          </div>
+                        )
+                      }
+                    </div>
                   </div>
                 </div>
                 :
                 <div>
                   {
-                    modatData?.cryptoData?.length > 1 && (
+                    modatData?.cryptoData?.length > 1 ? (
                       <div>
                         <p className="text-2xl text-center py-4 pb-4 ">{modatData?.cryptoTitle}</p>
                         <Divider text={'ORDER'} />
@@ -206,7 +240,7 @@ const Crypto = () => {
                                   modatData.cryptoData.map(item => <div onClick={() => {
                                     setMultiData({ ...item, cryptoTitle: modatData.cryptoTitle })
                                     setMultimodalOpen(true)
-
+                                    setDownState(false)
                                   }} className=" cursor-pointer flex justify-between gap-2">
                                     <p className="">{item.title}</p>
                                     <span className={`flex h-[30px]  px-3 items-center justify-center border text-sm py-0 rounded-lg ${item.stock > 0 ? "text-green-600  border-green-600" : 'text-slate-500'}`}> <AiFillDollarCircle />{item.price}USD</span>
@@ -220,6 +254,125 @@ const Crypto = () => {
                       </div>
 
                     )
+                      :
+                      <div>
+                        <p className="text-xl text-center">{modatData.cryptoTitle}</p>
+                        <div className="text-center text-sm flex justify-center">
+                          <span>min. Order:1/</span>
+                          <p>
+                            Stock: {modatData?.cryptoData?.stock} /
+                          </p>
+                          <p>
+                            Price:{modatData?.cryptoData?.price}
+                          </p>
+                        </div>
+                        <hr className="bg-slate-600 my-8" />
+                        <div>
+                          {/* header text */}
+                          {
+                            modatData?.cryptoData?.discription.whayNeed &&
+                            (
+                              <div>
+                                <p className="my-4 text-xl">Why Do You Need This Anyways?</p>
+                                <div >
+                                  {
+                                    modatData?.cryptoData?.discription.whayNeed.map(item => <li className="py-2 text-lg">{item}</li>)
+                                  }
+                                </div>
+                              </div>
+                            )
+                          }
+                          {
+                            modatData?.cryptoData?.discription.headerText && <p className="py-4 text-xl">{modatData?.cryptoData?.discription.headerText}</p>
+                          }
+                          {
+                            modatData?.cryptoData?.discription.comes_with && <p className="text-xl my-4">{modatData?.cryptoData?.discription.comes_with}</p>
+                          }
+                          {
+                            modatData?.cryptoData?.discription.middelText && <p className="text-xl my-4">{modatData?.cryptoData?.discription.middelText}</p>
+                          }
+                          {
+                            modatData?.cryptoData?.discription.country && <p className="text-xl my-4"> Country:{modatData?.cryptoData?.discription.country}</p>
+                          }
+                          {
+                            modatData?.cryptoData?.discription.afterPurchase && (
+                              <div>
+                                <p className="text-xl my-4">After Purchase what do I do?</p>
+                                {
+                                  modatData?.cryptoData?.discription.afterPurchase.map(item => <li className="text-lg my-4">{item}</li>)
+                                }
+                              </div>
+                            )
+
+
+                          }
+                          {
+                            modatData?.cryptoData?.discription.YouGet && <p className="text-xl my-4">you will get: {modatData?.cryptoData?.discription.YouGet}</p>
+                          }
+                          {
+                            modatData?.cryptoData?.discription.YouNotGet && <p className="text-xl my-4">What you will not get{modatData?.cryptoData?.discription.YouNotGet}</p>
+                          }
+                          {
+                            modatData?.cryptoData?.discription.specialNote && <p className="text-xl my-4">Special Note :{modatData?.cryptoData?.discription.specialNote}</p>
+                          }
+                          {
+                            modatData?.cryptoData?.discription.doesntComeWith && <p className="text-xl my-4">Doesn’t Come with : {modatData?.cryptoData?.discription.doesntComeWith}</p>
+                          }
+                          {
+                            modatData?.cryptoData?.discription.footerText && <p className="text-xl my-4">What you will not get{modatData?.cryptoData?.discription.footerText}</p>
+                          }
+                          {/* -- end */}
+                          {
+                            modatData?.cryptoData?.discription.telegramLink && (
+                              <div>
+                                {
+                                  modatData?.cryptoData?.discription.telegramLink.map(item => <div className="text-xl py-4">
+                                    <p>🔔Get FREE Access To Our Secret Channel:</p>
+                                    <a className="text-white hover:text-blue-300" href={item}>{item}</a>
+
+                                  </div>)
+                                }
+                              </div>
+                            )
+                          }
+                          <Divider text={'ORDER'} />
+
+                          <div className=" mt-2">
+                            {/* {
+                              downState && (
+                                <div className=" bg-[#11192C] p-4 rounded-md space-y-3">
+                                  {
+                                    modatData.cryptoData.map(item => <div onClick={() => {
+                                      setMultiData({ ...item, cryptoTitle: modatData.cryptoTitle })
+                                      setMultimodalOpen(true)
+                                      setDownState(false)
+
+                                    }} className=" cursor-pointer flex justify-between gap-2">
+                                      <p className="">{item.title}</p>
+                                      <span className={`flex h-[30px]  px-3 items-center justify-center border text-sm py-0 rounded-lg ${item.stock > 0 ? "text-green-600  border-green-600" : 'text-slate-500'}`}> <AiFillDollarCircle />{item.price}USD</span>
+                                    </div>)
+                                  }
+                                </div>
+                              )
+                            } */}
+
+
+                            <div>
+                              {
+                                modatData?.cryptoData?.stock > 0 && (
+                                  <div div className="flex items-center">
+                                    <span className="border border-stone-500 rounded-s-md py-2 px-4">Quantity</span>
+                                    <input onChange={(e) => setQuantity(e.target.value)} type="number" className="bg-transparent ps-3 rounded-e-md focus:border-blue-500 outline-none w-full border-2 py-2" defaultValue={multidata.stock} placeholder="350" />
+                                  </div>
+                                )
+                              }
+                              <button className={` bg-blue-600 ${modatData?.cryptoData?.stock <= 0 ? "cursor-not-allowed bg-blue-900 " : ""}  hover:bg-blue-500 w-full mt-4 py-2 rounded-md font-semibold`}>Check out for {quantity > 1 ? quantity * modatData?.cryptoData?.price : modatData?.cryptoData?.price} </button>
+                            </div>
+
+
+                          </div>
+                        </div>
+                      </div>
                   }
                 </div>
             }
